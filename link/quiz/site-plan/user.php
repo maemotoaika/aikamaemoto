369e6f2dce65a6951839033637381e27884d377a
前元　愛香 <112926287+maemotoaika@users.noreply.github.com>
2022-09-07 21:10:38 +0900

    Add files via upload

---
+++ link/quiz/site-plan/user.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html class="white">
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="userForm">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>USER</title>
+  <!-- Place favicon.ico in the root directory -->
+  <!-- <link href="https://fonts.googleapis.com/css?family=M+PLUS+1p:400,500" rel="stylesheet"> -->
+  <link href="css/style.css" rel="stylesheet">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+
+
+<header>
+  <nav>
+    <div class="nav">
+      <h1 class="first"></h1>
+      <ul class="hidden fir"></ul>
+    </div>
+    <div class="nav">
+      
+      <h1 class="second"></h1>
+      <ul class="hidden sec"></ul>
+    </div>
+    <div class="nav">
+      <h1 class="third"></h1>
+      <ul class="hidden thi"></ul>
+    </div>
+  </nav>
+</header>
+
+
+<main>
+</main>
+
+
+<footer>
+  <small><a href="../../index.html">PORTFOLIO_TOP</a></small>
+</footer>
+<script src="app.js"></script>
+</body>
+</html>
